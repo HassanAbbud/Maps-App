e17fd2aae63e88b72f21176064efc75e4958d564
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-17 14:25:37 -0600

    restructured to take viewChild instead of id for specific HTML elements local references

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import * as L from 'leaflet';
+import { AfterViewInit, Component, ElementRef, ViewChild, } from '@angular/core';
+import {Map, map, tileLayer} from 'leaflet';
+//import * as L from 'leaflet';
 
 @Component({
   selector: 'maps-full-screen-page',
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent implements OnInit{
-  private map?: L.Map;
+export class FullScreenPageComponent implements AfterViewInit{
+
+  //private map?: Map;
+  //HTML local reference "map"
+  @ViewChild('map')
+  public divMap?: ElementRef;
 
   initMap(){
-    this.map = L.map('map').setView([51.505, -0.09], 13);
-    const Layers = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    if(!this.divMap) throw "Element map wasn't found"
+    //Define the initial map
+    const myMap = map(this.divMap.nativeElement).setView([51.505, -0.09], 13);
+
+    //Define traversable layer tiles
+    const Layers = tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.map);
+    }).addTo(myMap);
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.initMap();
   }
 }
