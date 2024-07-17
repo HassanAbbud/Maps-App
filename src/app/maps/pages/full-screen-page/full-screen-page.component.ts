89840a41df1da2610801565fb54fcb1d702e5ce7
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-17 13:46:53 -0600

    Leaflet library & fullscreen map implementation

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
+  selector: 'maps-full-screen-page',
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements OnInit{
+  private map?: L.Map;
 
+  initMap(){
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    const Layers = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+  }
+
+  ngOnInit(): void {
+    this.initMap();
+  }
 }
