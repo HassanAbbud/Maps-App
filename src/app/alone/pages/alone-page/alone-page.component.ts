b154fa84575915a56c113584f37f4e44e1bedd18
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-18 13:27:26 -0600

    Standalone component and routing

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alone-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './alone-page.component.html',
+  styleUrl: './alone-page.component.css'
+})
+export class AlonePageComponent {
+
+}
